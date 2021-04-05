006d0acfe0f0e38717e2c32f5ec54d13f792fe03
bachdiep <29311886+bndiep@users.noreply.github.com>
2021-04-05 16:06:56 -0700

    adds flex box to skills section

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ function App() {
   return (
     <div className="m-8">
       <header>
-        <ul>
-          <li>Home</li>
-          <li>Skills</li>
-          <li>Design</li>
-          <li>Projects</li>
+        <ul className="flex flex-row justify-between">
+          <li><a href="/">Home</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#design">Design</a></li>
+          <li><a href="#projects">Projects</a></li>
         </ul>
       </header>
-      <h1 className="text-6xl">Hi, it's me, Bach, your friendly coder. </h1>
-      <div className="p-4 bg-gray-500 text-gray-100">
+      <h1 className="p-12 w-96 text-6xl">
+        Hi, I'm <span className="font-bold">Bach</span>, your friendly coder.
+      </h1>
+
+      <div className="p-8 bg-gray-500 text-gray-100">
         <p>
           I'm a full stack develop based in San Diego, CA.
           </p>
@@ -24,7 +27,7 @@ function App() {
           </p>
       </div>
 
-      <div>
+      <div className="p-4 flex justify-around">
         <button>Contact Me</button>
         <button>LinkedIn</button>
         <button>Twitter</button>
@@ -32,55 +35,67 @@ function App() {
         <button>Resume</button>
       </div>
 
-      <h2 className="text-3xl">Skills</h2>
-      <div>
-        <h3 className="text-2xl">Frontend</h3>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>Ruby</li>
-          <li>React</li>
-        </ul>
+      <h2 className="py-2 text-3xl" id="skills">Skills</h2>
+      <div className="flex flex-wrap justify-center">
+        <div className="w-48 p-2 border border-black rounded-lg">
+          <h3 className="text-2xl">Frontend</h3>
+          <ul>
+            <li className="">HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>Ruby</li>
+            <li>React</li>
+          </ul>
+        </div>
+        
+        <div className="w-48 p-2 border border-black rounded-lg">
+          <h3 className="text-2xl">Backend</h3>
+          <span className="text-xs">Heh. <span className="italic">"Bach-end"</span>. Sorry.</span>
+          <ul>
+            <li>Ruby on Rails</li>
+            <li>PostgreSQL</li>
+            <li>MySQL</li>
+          </ul>
+        </div>
+        
+        <div className="w-48 p-2 border border-black rounded-lg">
+          <h3 className="text-2xl">Version Control</h3>
+          <ul>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>GitLab</li>
+          </ul>
+        </div>
 
-        <h3 className="text-2xl">Backend</h3>
-        <span className="text-xs">Heh. Bach-end. Sorry.</span>
-        <ul>
-          <li>Ruby on Rails</li>
-          <li>PostgreSQL</li>
-          <li>MySQL</li>
-        </ul>
+        <div className="w-48 p-2 border border-black rounded-lg">
+          <h3 className="text-2xl">Remote Work</h3>
+          <ul>
+            <li>Slack</li>
+            <li>Zoom</li>
+          </ul>
+        </div>
 
-        <h3 className="text-2xl">Version Control</h3>
-        <ul>
-          <li>Git</li>
-          <li>GitHub</li>
-          <li>GitLab</li>
-        </ul>
-
-        <h3 className="text-2xl">Remote Work</h3>
-        <ul>
-          <li>Slack</li>
-          <li>Zoom</li>
-        </ul>
-
-        <h3 className="text-3xl">Design</h3>
-        <ul>
-          <li>Figma</li>
-          <li>Balsamiq</li>
-          <li>Adobe Illustrator</li>
-          <li>Affinity Designer</li>
-        </ul>
+        <div className="w-48 p-2 border border-black rounded-lg">
+          <h3 className="text-2xl" id="design">Design</h3>
+          <ul>
+            <li>Figma</li>
+            <li>Balsamiq</li>
+            <li>Adobe Illustrator</li>
+            <li>Affinity Designer</li>
+          </ul>
+        </div>
       </div>
 
-      <div>
-        <h2 className="text-3xl">Projects</h2>
+      <div className="">
+        <h2 className="py-2 text-3xl" id="projects">Projects</h2>
         <h4>LifeBand</h4>
         <h4>React Here, Tryna Function</h4>
         <h4>Tic-Tac-Toe</h4>
         <h4>Pig Latin</h4>
         <h4>Blog App</h4>
       </div>
+
+      <footer className="text-sm">Designed and build by Bach Diep using React.js and Tailwind CSS</footer>
     </div>
   )
 }
