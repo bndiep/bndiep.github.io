d7e970e4fff227eaec57cb2fe16dcefac90641c1
bachdiep <29311886+bndiep@users.noreply.github.com>
2021-04-06 17:20:50 -0700

    changes buttons to links and adds some responsiveness

--- src/App.js
+++ src/App.js
@@ -10,9 +10,14 @@ function App() {
           <li className="p-4"><a href="">Art</a></li>
         </ul>
       </header>
-      <h1 className="p-12 w-96 text-6xl">
-        Hi, I'm <span className="font-bold">Bach</span>, your friendly coder.
-      </h1>
+      <div className=" my-24 sm:my-72">
+        <h1 className="px-12 w-100 text-6xl">
+          Hi, I'm <span className="font-bold">Bach</span>,
+        </h1>
+        <h1 className="px-12 w-100 text-6xl">
+          your friendly coder.
+        </h1>
+      </div>
 
       <div className="p-8 bg-gray-500 text-gray-100 lg:mx-16">
         <p>
@@ -28,74 +33,82 @@ function App() {
       </div>
 
       <div className="p-4 flex justify-around">
-        <button>Contact Me</button>
-        <button>LinkedIn</button>
-        <button>Twitter</button>
-        <button>GitHub</button>
-        <button>Resume</button>
+        <a href="mailto:bachdiep.dev@gmail.com">Contact Me</a>
+        <a href="www.linkedin.com/in/bachdiep" target="_blank">LinkedIn</a>
+        <a href="https://twitter.com/itsabach" target="_blank">Twitter</a>
+        <a href="https://github.com/bndiep" target="_blank">GitHub</a>
+        <a href="" target="_blank">Resume</a>
       </div>
 
-      <h2 className="py-2 text-3xl" id="skills">Skills</h2>
-      <div className="flex flex-wrap justify-center items-start">
-        
-        <div className="m-2 w-48 border-t border-l border-r border-black rounded-lg">
-          <h3 className="p-2 h-48 text-2xl border-b-2 border-black rounded-lg">Frontend</h3>
-          <ul className="">
-            <li className="p-2 border-b-2 border-black rounded-lg">HTML</li>
-            <li className="p-2 border-b-2 border-black rounded-lg">CSS</li>
-            <li className="p-2 border-b-2 border-black rounded-lg">JavaScript</li>
-            <li className="p-2 border-b-2 border-black rounded-lg">Ruby</li>
-            <li className="p-2 border-b-2 border-black rounded-lg">React</li>
-          </ul>
-        </div>
-        
-        <div className="m-2 w-48 border-t border-l border-r border-black rounded-lg">
-          <h3 className="p-2 h-48 text-2xl border-b-2 border-black rounded-lg">Backend</h3>
-          <ul>
-            <li className="p-2 border-b-2 border-black rounded-lg">Ruby on Rails</li>
-            <li className="p-2 border-b-2 border-black rounded-lg">PostgreSQL</li>
-            <li className="p-2 border-b-2 border-black rounded-lg">MySQL</li>
-          </ul>
-        </div>
-        
-        <div className="m-2 w-48 border-t border-l border-r border-black rounded-lg">
-          <h3 className="p-2 h-48 text-2xl border-b-2 border-black rounded-lg">Version Control</h3>
-          <ul>
-            <li className="p-2 border-b-2 border-black rounded-lg">Git</li>
-            <li className="p-2 border-b-2 border-black rounded-lg">GitHub</li>
-            <li className="p-2 border-b-2 border-black rounded-lg">GitLab</li>
-          </ul>
-        </div>
+      <div className="">
+        <h2 className="py-2 text-3xl font-bold border-b-2" id="skills">Skills</h2>
+        <div className="flex flex-wrap justify-center items-start">
+          
+          <div className="m-2 w-48 border-t border-l border-r border-black rounded-lg">
+            <h3 className="p-2 h-48 text-2xl border-b-2 border-black rounded-lg">Frontend</h3>
+            <ul className="">
+              <li className="p-2 border-b-2 border-black rounded-lg">HTML</li>
+              <li className="p-2 border-b-2 border-black rounded-lg">CSS</li>
+              <li className="p-2 border-b-2 border-black rounded-lg">JavaScript</li>
+              <li className="p-2 border-b-2 border-black rounded-lg">Ruby</li>
+              <li className="p-2 border-b-2 border-black rounded-lg">React</li>
+            </ul>
+          </div>
+          
+          <div className="m-2 w-48 border-t border-l border-r border-black rounded-lg">
+            <h3 className="p-2 h-48 text-2xl border-b-2 border-black rounded-lg">Backend</h3>
+            <ul>
+              <li className="p-2 border-b-2 border-black rounded-lg">Ruby on Rails</li>
+              <li className="p-2 border-b-2 border-black rounded-lg">PostgreSQL</li>
+              <li className="p-2 border-b-2 border-black rounded-lg">MySQL</li>
+            </ul>
+          </div>
+          
+          <div className="m-2 w-48 border-t border-l border-r border-black rounded-lg">
+            <h3 className="p-2 h-48 text-2xl border-b-2 border-black rounded-lg">Version Control</h3>
+            <ul>
+              <li className="p-2 border-b-2 border-black rounded-lg">Git</li>
+              <li className="p-2 border-b-2 border-black rounded-lg">GitHub</li>
+              <li className="p-2 border-b-2 border-black rounded-lg">GitLab</li>
+            </ul>
+          </div>
 
-        <div className="m-2 w-48 border-t border-l border-r border-black rounded-lg">
-          <h3 className="p-2 h-48 text-2xl border-b-2 border-black rounded-lg">Remote Work</h3>
-          <ul>
-            <li className="p-2 border-b-2 border-black rounded-lg">Slack</li>
-            <li className="p-2 border-b-2 border-black rounded-lg">Zoom</li>
-          </ul>
-        </div>
+          <div className="m-2 w-48 border-t border-l border-r border-black rounded-lg">
+            <h3 className="p-2 h-48 text-2xl border-b-2 border-black rounded-lg">Remote Work</h3>
+            <ul>
+              <li className="p-2 border-b-2 border-black rounded-lg">Slack</li>
+              <li className="p-2 border-b-2 border-black rounded-lg">Zoom</li>
+            </ul>
+          </div>
 
-        <div className="m-2 w-48 border-t border-l border-r border-black rounded-lg">
-          <h3 className="p-2 h-48 text-2xl border-b-2 border-black rounded-lg">Design</h3>
-          <ul>
-            <li className="p-2 border-b-2 border-black rounded-lg">Figma</li>
-            <li className="p-2 border-b-2 border-black rounded-lg">Balsamiq</li>
-            <li className="p-2 border-b-2 border-black rounded-lg">Adobe Illustrator</li>
-            <li className="p-2 border-b-2 border-black rounded-lg">Affinity Designer</li>
-          </ul>
+          <div className="m-2 w-48 border-t border-l border-r border-black rounded-lg">
+            <h3 className="p-2 h-48 text-2xl border-b-2 border-black rounded-lg">Design</h3>
+            <ul>
+              <li className="p-2 border-b-2 border-black rounded-lg">Figma</li>
+              <li className="p-2 border-b-2 border-black rounded-lg">Balsamiq</li>
+              <li className="p-2 border-b-2 border-black rounded-lg">Adobe Illustrator</li>
+              <li className="p-2 border-b-2 border-black rounded-lg">Affinity Designer</li>
+            </ul>
+          </div>
         </div>
       </div>
 
       <div className="">
-        <h2 className="py-2 text-3xl" id="projects">Projects</h2>
-        <h4>LifeBand</h4>
-        <h4>React Here, Tryna Function</h4>
-        <h4>Tic-Tac-Toe</h4>
-        <h4>Pig Latin</h4>
-        <h4>Blog App</h4>
+        <h2 className="py-2 text-3xl font-bold" id="projects">Projects</h2>
+        <div className="flex flex-wrap justify-center items-start">
+          <h4 className="p-2 h-48 text-2xl border-b-2 border-black rounded-lg">LifeBand</h4>
+          <div className="m-2 w-48 border-t border-l border-r border-black rounded-lg">
+            
+          </div>
+          
+          <h4 className="p-2 h-48 text-2xl border-b-2 border-black rounded-lg">React Here, Tryna Function</h4>
+          <h4 className="p-2 h-48 text-2xl border-b-2 border-black rounded-lg">Tic-Tac-Toe</h4>
+          <h4 className="p-2 h-48 text-2xl border-b-2 border-black rounded-lg">Pig Latin</h4>
+          <h4 className="p-2 h-48 text-2xl border-b-2 border-black rounded-lg">Blog App</h4>
+        </div>
       </div>
 
-      <footer className="mx-2 text-sm flex justify-center">Designed and built by Bach Diep using React.js and Tailwind CSS</footer>
+      <footer className="px-2 text-sm flex justify-center">Designed and built by Bach Diep using React.js and Tailwind CSS</footer>
     </div>
   )
 }
